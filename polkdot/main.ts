bca3891fbf1356cc22c1c0c1636c6793141210eb
bingone <277835572@qq.com>
2024-04-18 19:59:03 +0800

    1.添加事件订阅 2.添加转账

--- polkdot/main.ts
+++ polkdot/main.ts
@@ -1,4 +1,7 @@
-import { ApiPromise,WsProvider} from "@polkadot/api";
+import { ApiPromise,WsProvider,Keyring} from "@polkadot/api";
+import "@polkadot/api-augment";
+import type {FrameSystemAccountInfo} from "@polkadot/types/lookup";
+import {KeyringPair} from "@polkadot/keyring/types";
 const WEB_SOCKET="ws://127.0.0.1:9944";
 const connect=async () => {
 const wsProvider=new WsProvider(WEB_SOCKET);
@@ -7,15 +10,42 @@ await api.isReady;
 return api;
 
 }
+const sleep=(ms:number)=>new Promise(r => setTimeout(r,ms));
 const getConst=async (api:ApiPromise) =>{
     const con=await api.consts.balances.existentialDeposit.toHuman();
 	return con;
 }
+const getFree=async(api:ApiPromise,address:string)=>{
+	const {data:{free,},}:FrameSystemAccountInfo =await
+	api.query.system.account(address);
+	return free;
+}
+const transfer=async(api:ApiPromise,from:KeyringPair,to:string,count:number)=>{
+	await api.tx.balances.transfer(to,count).signAndSend(from,res => {
+		console.log("transfer=",res.status);
+	});
+} 
+const subcrib=async(api:ApiPromise,address:string)=>{
+	await api.query.system.account(address,info=>{
+		const free=info.data.free;
+		console.log("sub fre=",free.toHuman());
+	})
+
+}
 const main = async () =>{
 const api=await connect();
 const deposit=await getConst(api);
+const keyring=new Keyring({type:"sr25519"});
+const alice=keyring.addFromUri("//Alice");
+const bob=keyring.addFromUri("//Bob");
+const free=await getFree(api,bob.address);
 console.log("deposit==",deposit);
-
+console.log("free=",free.toHuman());
+await subcrib(api,alice.address);
+await transfer(api,alice,bob.address,10 ** 10 +1);
+await sleep(10000);
+const after_free=await getFree(api,bob.address);
+console.log("free=",after_free.toHuman());
 }
 main().then(() =>{
 console.log("then");
--- polkdot/readme.txt
+++ polkdot/readme.txt
@@ -2,3 +2,5 @@
 npm install -g ts-node
 yarn add polkadot
 yarn add @polkadot/api
+yarn add @polkadot/types
+yarn add @polkadot/api-augment
